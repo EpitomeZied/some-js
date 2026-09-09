ed88f4afda11a405ed983d19fccdf6a2ce9cd1ed
Mahmoud AboZied <epitomezied@gmail.com>
2026-09-09 17:37:15 +0300

    done

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-// Print the greeting on a single line below.
-console.log('Hello, JavaScript!');
+const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+const a = Number(lines[0]);
+const b = Number(lines[1]);
+console.log(a + b)
