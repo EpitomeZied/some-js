2ea8842bcc8faaf7325a044f34f6423bbdff97eb
Mahmoud AboZied <epitomezied@gmail.com>
2026-09-09 17:44:37 +0300

    done

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
 const a = Number(lines[0]);
 const b = Number(lines[1]);
-console.log(a + b)
+const c = Number(lines[2]);
+// Print the floor of the average below.
+console.log(Math.floor((a + b + c) / 3));
