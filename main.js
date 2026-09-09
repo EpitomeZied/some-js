4b09c8a4f2e88593e75f27856d324dde38ff321b
Mahmoud AboZied <epitomezied@gmail.com>
2026-09-09 18:43:07 +0300

    done

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
 const n = Number(require('fs').readFileSync(0, 'utf-8').trim());
 // Print FizzBuzz / Fizz / Buzz / n based on divisibility.
-if(n%5 === 0 && n%3 === 0){
-    console.log("FizzBuzz")
-}
-
-else if(n%3 === 0 ) {
-    console.log("Fizz")
-}
-else if(n%5 === 0){
-    console.log("Buzz")
-}
-else {
-    console.log(n)
-}
+console.log(n * (n + 1) / 2)
